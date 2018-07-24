3698baa627dc4110c1d270faad2f19a2ed9041a4
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-24 14:42:13 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -57,7 +57,25 @@ class TweetsController < ApplicationController
  end
 end
 
-  # patch 'tweets/:id' do
-  #   @tweet = Tweet.
-  # end
+  patch 'tweets/:id' do
+    if logged_in? 
+      if params[:content]== "" 
+      redirect to '/tweets/#{tweet.id}/edit'
+    else 
+      @tweet= Tweet.find_by_id(:id)
+      if @tweet && @tweet.user == current_user 
+        if @tweet.update(params[:content])
+          redirect to :'/tweets/#{@tweet.id}' 
+        else redirect to :'/tweets/#{@tweet.id}/edit'
+        end 
+      else
+        redirect to '/tweets'
+      end 
+    end 
+  else 
+    redirect to '/login'
+  end 
+end  
+    
+  end
 end
