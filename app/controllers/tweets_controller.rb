563943df4e643111ec67b78bd72e1c09ef03e192
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-24 14:42:55 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -59,23 +59,23 @@ end
 
   patch 'tweets/:id' do
     if logged_in?
-      if params[:content]== ""
-      redirect to '/tweets/#{tweet.id}/edit'
-    else
-      @tweet= Tweet.find_by_id(:id)
-      if @tweet && @tweet.user == current_user
-        if @tweet.update(params[:content])
-          redirect to :'/tweets/#{@tweet.id}'
-        else redirect to :'/tweets/#{@tweet.id}/edit'
-        end
-      else
-        redirect to '/tweets'
-      end
-    end
-  else
-    redirect to '/login'
-  end
-end
-
+         if params[:content] == ""
+           redirect to "/tweets/#{params[:id]}/edit"
+         else
+           @tweet = Tweet.find_by_id(params[:id])
+           if @tweet && @tweet.user == current_user
+             if @tweet.update(content: params[:content])
+               redirect to "/tweets/#{@tweet.id}"
+             else
+               redirect to "/tweets/#{@tweet.id}/edit"
+             end
+           else
+             redirect to '/tweets'
+           end
+         end
+       else
+         redirect to '/login'
+       end
+     end
   
 end
