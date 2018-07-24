a8a8a42c05796330f942751d0a6d73012156937d
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-07-24 14:21:11 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -36,9 +36,11 @@ class TweetsController < ApplicationController
   end
 
   get '/tweets/:id' do
-    @tweet= Tweet.find_by_id(params[:id])
-
-    erb :'/tweets/show'
+    if logged_in? 
+      @tweet= Tweet.find_by_id(params[:id]
+      erb :'/tweets/show'
+    else 
+      erb :'/login'
 
   end
 
